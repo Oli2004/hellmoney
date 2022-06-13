aac81737daad3588d5e2f9a26b64c17649f3a68f
Oli2004 <30935873+Oli2004@users.noreply.github.com>
2022-06-13 12:22:59 -0400

    Update startminer.sh

--- startminer.sh
+++ startminer.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBcoSFZsD2TraxEjaVy5xFmc7UFjDu1XAi.Cluster -p x --cpu 8
